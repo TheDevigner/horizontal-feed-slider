9d0e5763d7df300073f89fdd0ec7a760446d8f2f
TheDevigner <addevigner@gmail.com>
2019-01-17 20:42:23 +0100

    Add files via upload
    * fix the rows and columns to 1 on mobile viewport size.
    * add Slider class.

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
 
-// slider computed properties from styling file
-const styles = getComputedStyle(document.body);      
-const columnWidth = styles.getPropertyValue('--shift-slid');
-const columnsStyle = parseInt(styles.getPropertyValue('--columns'));
-const rowsStyle = parseInt(styles.getPropertyValue('--rows')); 
-
 // Slider elements
 const nextButton = document.querySelector('#next');
 const previousButton = document.querySelector('#previous');
@@ -12,77 +6,42 @@ const sliderGrid = document.querySelector('#slider');
 const sliderProgressBar = document.querySelector('#slider-progress-bar');
 
 
-// get url parameters or set to default
+// get url parameters or set slider properties to default (2 columns 3 rows)
 const urlParams = new URLSearchParams(window.location.search);
-const columns = urlParams.get('columns') || columnsStyle;
-const rows = urlParams.get('rows') || rowsStyle ;
-const autoplay = urlParams.get('autoplay') || false;
+const columns = urlParams.get('columns') || 3;
+const rows = urlParams.get('rows') || 2;
+const auto_play = urlParams.get('autoplay') || 'true';
+const total_items = sliderGrid.childElementCount;
 
-// init slider rows and columns
-const docElementStyle = document.documentElement.style;
-docElementStyle.setProperty('--rows', rows);
-docElementStyle.setProperty('--columns', columns);
-
-// Slider properties
-const GRID_LENGTH = sliderGrid.childElementCount; 
-const GRID_COLUMNS = parseInt(columns); 
-const GRID_ROWS = parseInt(rows);
-const FIRST_SLIDE = 0; 
-const GRID_CELLS_COUNT = GRID_ROWS * GRID_COLUMNS;
-const SLIDES_COUNT = Math.ceil((GRID_LENGTH - GRID_CELLS_COUNT) / GRID_ROWS);
-
-let currentSlide = 0;
-let slidShift = 0;
-
-console.log('Grid Length :' + GRID_LENGTH);
-console.log('Slider size : ' + SLIDES_COUNT);
-console.log('Grid Cells :' + GRID_CELLS_COUNT);
+let slider = new Slider(columns, rows, auto_play, total_items);
+slider.render();
 
 // move to the Next Slide on next button click
-nextButton.addEventListener('click', ()=>{
-    nextSlide();
+nextButton.addEventListener('click', (e)=>{
+    slider.nextSlide();
+    e.preventDefault();
 } );
 
 // move to the previous Slide on previous button click
-previousButton.addEventListener('click', ()=>{
-    previousSlide();
+previousButton.addEventListener('click', e =>{
+    slider.previousSlide();
+    e.preventDefault();
 } );
 
+// autoplay
+let autoplay = slider.autoPlayOn() ? setInterval(() =>{
+    slider.nextSlide();
+}, 3000) : 0;
 
-let nextSlide = () => {
-    // check if there is still content to preview
-    if (!lastSlide()){
-        // shift content to the left
-        docElementStyle.setProperty('--current-slide', --slidShift);
-        
-        // update slider progress bar
-        sliderProgressBar.style.width = ++currentSlide / SLIDES_COUNT * 100 + '%';
-    }
+// update on slider properties on viewport change fix columns and rows to 1
+var mql = window.matchMedia('(max-width: 600px)');
+function screenTest(e) {
+  /* the viewport is less than than 600 pixels wide */
+    if (e.matches) 
+        slider.renderMobileView();
+    else 
+    /* the viewport is more than than 600 pixels wide */
+        slider.render();
 }
-
-let previousSlide = () =>{
-    // Check the slider is not on the first Slide
-    if( !firstSlide() ){
-        // shift slid one block to the right
-        docElementStyle.setProperty('--current-slide', ++slidShift);  
-        
-        // update slider progress bar and current slide number
-        sliderProgressBar.style.width = --currentSlide / SLIDES_COUNT * 100 + '%';
-    }
-}
-
-let lastSlide = () => {
-    return currentSlide === SLIDES_COUNT;
-}
-
-let firstSlide = () =>{
-    return currentSlide  === FIRST_SLIDE;
-}
-
-let autoPlayOn = () => {
-    return autoplay === 'true';
-}
-
-let autoPlay = autoPlayOn() ? setInterval(nextSlide, 3000) : 0;
-
+mql.addListener(screenTest);
 
